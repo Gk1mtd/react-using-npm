f63f909dc40b55499e04ae2b9e014c763c64a5dd
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-09 11:14:36 +0100

    done

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react"
+// src/app.js
+// const App = () => {
+//     return <h1 className="app">Hello World!</h1>;
+//   };
+
+  //oder
+  const App = () => <h1 className="app">Hello World!!!</h1>
+
+  export default App
